1e307915a90ec954cf17101c271ffc3059a4dead
ditinagrawal <ditin.agrawal05@gmail.com>
2025-08-20 18:58:49 +0530

    Day 4

--- app/learn/day-4/page.jsx
+++ app/learn/day-4/page.jsx
@@ -7,34 +7,33 @@ import Link from "next/link";
 
 gsap.registerPlugin(useGSAP);
 
-export default function DayThree() {
+export default function DayFour() {
   useGSAP(() => {
-    gsap.to(".box", {
-      y: 100,
-      stagger: 0.1,
-      repeat: -1,
-      yoyo: true,
-    });
-    gsap.to(".box", {
-      y: 20,
-      stagger: 0.1,
-      repeat: -1,
-      yoyo: true,
-    });
+    let tl = gsap.timeline({ yoyo: true, repeat: -1 });
+    tl.to(".box2", {
+      y: 50,
+      ease: "power2.inOut",
+    })
+      .to(".box1", {
+        y: 50,
+        ease: "power2.inOut",
+      })
+      .to(".box3", {
+        y: 50,
+        ease: "power2.inOut",
+      });
   });
   return (
     <div className="flex flex-col gap-10 items-center justify-center h-full">
       <div className="flex gap-6">
-        <div className="w-16 h-16 bg-yellow-600 rounded-full box" />
-        <div className="w-16 h-16 bg-red-600 rounded-full box" />
-        <div className="w-16 h-16 bg-green-600 rounded-full box" />
-        <div className="w-16 h-16 bg-blue-600 rounded-full box" />
-        <div className="w-16 h-16 bg-purple-600 rounded-full box" />
+        <div className="w-16 h-16 bg-yellow-600 rounded-full box1" />
+        <div className="w-16 h-16 bg-red-600 rounded-full box2" />
+        <div className="w-16 h-16 bg-green-600 rounded-full box3" />
       </div>
       <div className="absolute bottom-0 left-0 w-full flex justify-center gap-2 h-10 items-center text-sm">
         Resources used to create this lesson -{" "}
         <Link
-          href="https://gsap.com/resources/getting-started/Staggers"
+          href="https://gsap.com/resources/getting-started/timelines"
           target="_blank"
           className="text-blue-500 flex items-center gap-1"
         >
